9dddf1e5d563a521c3360d1d55c9ebc90cb1a174
DeeGn <dan@livinglifehomeimprovements.com>
2021-06-05 17:46:35 +0700

    format files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from "react"
-import { Container } from "react-bootstrap"
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { AuthProvider } from "./contexts/AuthContext"
+import { AuthProvider } from "./contexts/AuthContext";
 
-import Signup from "./views/auth/Signup"
-import Explore from "./views/explore/Explore"
-import Login from "./views/auth/Login"
-import ForgotPassword from "./views/auth/ForgotPassword"
-import UpdateProfile from "./views/auth/UpdateProfile"
+import Signup from "./views/auth/Signup";
+import Explore from "./views/explore/Explore";
+import Login from "./views/auth/Login";
+import ForgotPassword from "./views/auth/ForgotPassword";
+import UpdateProfile from "./views/auth/UpdateProfile";
 
-import PrivateRoute from "./routes/PrivateRoute"
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
-  
     <Container
       className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}
     >
-      <div className="w-100" style={{ maxWidth: '400px' }}>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
         <Router>
           <AuthProvider>
             <Switch>
@@ -33,7 +32,7 @@ function App() {
         </Router>
       </div>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
